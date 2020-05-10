f1d0f4bb4247f02c0837872cb10c27ca3a6c27bd
sampletext32 <sampletext32@bk.ru>
2020-05-11 02:47:00 +0300

    Add project

---
+++ Manucharyan_6/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double min(double a, double b)
+{
+	double t;
+	if (a <= b)
+	{
+		t = a;
+	}
+	else
+	{
+		t = b;
+	}
+	return t;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	double a, b, c, d;
+
+	cout << "Введите A: ";
+	cin >> a;
+	cout << "Введите B: ";
+	cin >> b;
+	cout << "Введите C: ";
+	cin >> c;
+	cout << "Введите D: ";
+	cin >> d;
+
+	double r = (min(a, b) + min(c, d)) / min(1.5, abs(d));
+	cout << "Результат: " << r << "\n";
+
+	system("pause");
+
+	return 0;
+}
